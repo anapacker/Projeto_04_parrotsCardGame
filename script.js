d272fed3bb560ab51c1ef0e85468df60e0319d9b
Ana Packer <anaamaria.packer@gmail.com>
2022-10-20 17:34:23 -0300

    imagens adicionada ao js

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+let numDeCartas;
+
+const imagens = [
+    'bobrossparrot.gif',
+    'explodyparrot.gif',
+    'fiestaparrot.git',
+    'metalparrot.gif',
+    'revetitparrot.gif',
+    'tripletsparrot.gif',
+    'unicornparrots.gif'
+
+];
+
 function perguntarQtdCartas() {
-    prompt('Com quantas cartas deseja jogar?');
+    numDeCartas = Number(prompt('Com quantas cartas deseja jogar?'));
+
+
+    while (numDeCartas < 4 || numDeCartas > 14 || (numDeCartas % 2 !== 0)) {
+        alert('Quantidade cartas inválida!');
+        numDeCartas = Number(prompt('Com quantas cartas deseja jogar?'));
+
+
+    }
 }
+perguntarQtdCartas()
+
+
+function mostrarCartas{
+
+
+}
+
+    // const numDeCartas = [1]
+
+
+    ///precisa ser um numero
+    //precisa ser par
+    //precisa repetir até ser um numero válio
+
+
+
+
+
